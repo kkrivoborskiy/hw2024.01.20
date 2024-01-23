113de008ea37fa90a8d6c1bc28634f34e5e298e3
Kirill <kku23102001@gmail.com>
2024-01-23 21:58:44 +0300

    added counter of array elements

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
 ﻿string[] array = new string[] {"plus", "cat and dog", "+24", "hi", "world", "no!", "3"};
+int counter = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+        counter++;
+}
