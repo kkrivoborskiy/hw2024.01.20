37f5c4fc94caa71a7066c6a3e9cdffadd54d9b8f
Kirill <kku23102001@gmail.com>
2024-01-23 22:18:35 +0300

    added second array

--- Program.cs
+++ Program.cs
@@ -6,3 +6,12 @@ for (int i = 0; i < array.Length; i++)
     if (array[i].Length < 4)
         counter++;
 }
+string[] array2 = new string[counter];
+int j = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length < 4)
+    {
+        array2[j] = array[i];
+        j++;
+    }
